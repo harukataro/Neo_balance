c2113d4102f494a2ca7735f0d54935c94c835455
haruka <36725428+harukataro@users.noreply.github.com>
2018-08-25 18:43:23 +0900

    Add files via upload
    added transaction history fetch and calculate old block balance

--- neoBalanceGet.py
+++ neoBalanceGet.py
@@ -6,18 +6,18 @@ import json
 
 def dataGet(address):
 
-    print ("NEO address: ", end="")
-    print(address)
+
     # neoscan
     url = 'https://neoscan.io/'
+    targetBlock = 2640000
 
     #APIextention
     NeoGetBalance='api/main_net/v1/get_balance/'
-    NeoGetHight='api/main_net/v1/get_height/'
+    NeoGetTran='api/main_net/v1/get_address_abstracts/'
 
     # urlの作成
     paramStrB = url+NeoGetBalance+address
-    paramStrH = url+NeoGetHight
+    paramStrT = url+NeoGetTran+address+'/1'
 
     response= requests.get(paramStrB)
     content = response.json()
@@ -25,19 +25,35 @@ def dataGet(address):
     #特定のキーの残高の抽出
     for key in content['balance']:
         if key['asset'] == "NEO":
-                    print("NEO Balance is ",key['amount']),
+                    Balance = key['amount']
 
-    response= requests.get(paramStrH)
+
+    response=requests.get(paramStrT)
     content = response.json()
-    print("Block Heihgt is ",content['height']),
 
-    print("------------------")
+    for key in content['entries']:
+            #print(key['block_height'],key['amount'],key['address_to'])
+            if key['block_height'] > targetBlock:
+                if key['address_to'] == address:
+                    amount = float(key['amount'])
+                    Balance = Balance + amount
+                    #print(Balance)
+                else:
+                    Balance = Balance - amount
+                    #print(Balance)
+    return Balance
 
 if __name__ == '__main__':
 
     #NEO Address
     address1='AXDt2hzT35knLnV3MB3dR9rAvYmadUfVdb'
     address2='AKnbvRwL1MSPFWoS6bdD5v2SNHq2uta5tm'
-    print("------------------")
+    print("==================")
     resStr = dataGet(address1)
+    print("NEO address: ",address1)
+    print("Balances: ",resStr)
+    print("==================")
     resStr = dataGet(address2)
+    print("NEO address: ",address2)
+    print("Balances: ",resStr)
+    print("==================")
