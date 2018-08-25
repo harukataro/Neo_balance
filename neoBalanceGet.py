d427e032f63fdcdf59e59faa9f95d2fe767b60bc
haruka <36725428+harukataro@users.noreply.github.com>
2018-08-25 17:23:53 +0900

    Add files via upload

--- neoBalanceGet.py
+++ neoBalanceGet.py
@@ -6,6 +6,8 @@ import json
 
 def dataGet(address):
 
+    print ("NEO address: ", end="")
+    print(address)
     # neoscan
     url = 'https://neoscan.io/'
 
@@ -23,19 +25,19 @@ def dataGet(address):
     #特定のキーの残高の抽出
     for key in content['balance']:
         if key['asset'] == "NEO":
-                    print(key['amount'])
-
+                    print("NEO Balance is ",key['amount']),
 
     response= requests.get(paramStrH)
     content = response.json()
-    print(content['height'])
+    print("Block Heihgt is ",content['height']),
 
+    print("------------------")
 
 if __name__ == '__main__':
 
     #NEO Address
     address1='AXDt2hzT35knLnV3MB3dR9rAvYmadUfVdb'
     address2='AKnbvRwL1MSPFWoS6bdD5v2SNHq2uta5tm'
-
+    print("------------------")
     resStr = dataGet(address1)
-#    resStr = dataGet(address2)
+    resStr = dataGet(address2)
